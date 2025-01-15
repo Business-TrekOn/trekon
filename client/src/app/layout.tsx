1754de594be4517cdab70efde7c3bc33758804cf
Business-TrekOn <business.trekon@gmail.com>
2025-01-15 09:45:30 +0530

    feat: Add next ui and other stuffs like licenses

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "TrekOn",
+  description: "Where Every Step Tells a Story",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
